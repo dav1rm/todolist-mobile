73484b5002430653c6668c87c67b7f9387761dc9
Davi <davirodrigues8@gmail.com>
2019-03-05 18:28:34 -0300

    Adiciona validação no add todo

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -48,6 +48,11 @@ export default class TodoList extends Component {
   handleAddTodo = async () => {
     const { newTodo, todos } = this.state;
 
+    if (newTodo.trim().length == 0) {
+      alert("Valor do campo inválido");
+      return;
+    }
+
     try {
       const { data } = await api.post("/todos", { content: newTodo });
 
